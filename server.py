2ee3b7f56c5d62994ae5fa63d839efc1b10b9b2f
sasha42 <sasha42@users.noreply.github.com>
2020-12-28 18:53:38 +0100

    Update server logic and frontend parameters

--- server.py
+++ server.py
@@ -6,9 +6,35 @@ import os
 from flask_cors import CORS
 
 
-r = redis.from_url(os.environ.get("REDIS_URL"))
+r = redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
 app = Flask(__name__)
 CORS(app)
+
+
+def checkAccess(request):
+    '''Checks if the user can make this change'''
+
+    # Check the IP of the requester
+    request_ip = request.remote_addr
+    ips = request_ip.split('.')
+    subnet = f'{ips[0]}.{ips[1]}.{ips[2]}.0'
+
+    # Checks if the "ALLOWED_IPS" environment variable contains IPs,
+    # if it does, then checks if requester IP is in the allowed
+    # list. The env var should be a comma separated list, e.g.
+    # ALLOWED_IPS = "127.0.0.1,123.123.123.123"
+    allowed_ips = os.getenv("ALLOWED_IPS", "")
+
+    if allowed_ips != "":
+        ips = allowed_ips.split(',')
+
+        if subnet in ips:
+            return True # Return true if the user is in allowed list
+        else:
+            return False
+
+    else:
+        return True # Always return true if we do not have a list
 
 
 @app.route('/')
@@ -18,13 +44,8 @@ def index():
 
 @app.route('/action', methods=['POST'])
 def action():
-    # Check the IP of the requester
-    request_ip = request.remote_addr
-    ips = request_ip.split('.')
-    subnet = f'{ips[0]}.{ips[1]}.{ips[2]}.0'
-
-    # Only change the mode if the IP is OK
-    if subnet in ['127.0.0.0', '62.220.135.0', '192.168.130.0']:
+    # If user has access, let them change the mode
+    if checkAccess(request) == True:
         print(f'Changing to {request.form["mode"]}')
 
         mode = {'mode': request.form['mode']}
@@ -42,11 +63,28 @@ def status():
     return jsonify(mode)
 
 
+@app.route('/params', methods=['GET', 'POST'])
+def params():
+    # If user has access, let them change the mode
+    if checkAccess(request) == True:
+        if request.method == 'GET':
+            p_mode = r.get('params')
+            params = pickle.loads(p_mode)
+            return jsonify(params)
+    else:
+        return 'access denied'
+
+
 if __name__ == "__main__":
     # seed an initial value
     mode = {'mode': 'general'}
     p_mode = pickle.dumps(mode)
     r.set('mode', p_mode)
+    r.set('params', p_mode) # hack
+
+    # set up ssl
+    #context = ('/home/pi/Music-LED/cert.crt', '/home/pi/Music-LED/cert.key')
 
     # run server
-    app.run(debug=False, host="0.0.0.0")
+    #app.run(debug=True, host="0.0.0.0", port=443, ssl_context=context)
+    app.run(debug=True, host="0.0.0.0")
